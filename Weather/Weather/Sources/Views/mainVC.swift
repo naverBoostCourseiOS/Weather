a7add395985a0df674cadd03dad15fdf78e5459a
doyeonjeong <debby_@kakao.com>
2023-02-20 19:10:10 +0900

    🔥 Set No Storyboard Environment

--- Weather/Weather/Sources/Views/mainVC.swift
+++ Weather/Weather/Sources/Views/mainVC.swift
@@ -8,8 +8,31 @@
 import UIKit
 
 class mainVC: UIViewController {
-
+    
+    let textLabel : UILabel = {
+        let text = UILabel()
+        
+        text.text = "No Storyboard Check"
+        text.textColor = .systemBlue
+        text.translatesAutoresizingMaskIntoConstraints = false
+        
+        return text
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .white
+        self.view.addSubview(textLabel)
+        
+        textLabelAutoLayout()
+    }
+}
+
+extension mainVC {
+    func textLabelAutoLayout() {
+        NSLayoutConstraint.activate([
+            self.textLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.textLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+        ])
     }
 }
